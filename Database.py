6b65bed0eb69c8a53ef40aa759ee708eb8201de7
David Anapolsky <72574712+PlasmaRaptor360@users.noreply.github.com>
2021-09-10 17:21:42 -0500

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,12 @@
+"""
+Request a Charger
+Database Code
+David Anapolsky
+"""
+
+# Import the pandas module to create tables to manage the data
+import pandas as pd
+
+# Define the main() function
+def main():
+    pass
