c9ab2d21ff4b7284833d65ecdddb9bbbbbdc1307
David <davidaanapolsky@gmail.com>
2021-09-24 23:43:35 -0500

    Object-Oriented Database

--- Database.py
+++ Database.py
@@ -7,120 +7,284 @@ David Anapolsky
 # Import the pandas module to create tables to manage the data
 import pandas as pd
 
-# Define the main function
-def main():
-    # Store the filename of the user data in a variable
-    userDataFile = 'userData.dat'
+# Define the demo function for testing/demonstration purposes
+def demo():
+    # Store the filename of the student data in a variable
+    studentDataFile = 'studentData.dat'
 
-    # Setup the user data dataframe
-    userDataFrame = setup_user_data(userDataFile)
+    # Create an instance of the Database class
+    database = Database(studentDataFile)
 
-    #Temp test adding data
-    userDataFrame = add_user_data(userDataFrame, 'epresent@emeryweiner.org','ad334f', 1, 'USB-C', 1)
-    print(userDataFrame.head())
+    # Testing adding student data normally
+    success = database.add_student('epresent@emeryweiner.org','ad334f', 1, 'USB-C', 1)
+    print('\nDatabase:', database, '\nSuccess:', success)
 
-    userDataFrame = add_user_data(userDataFrame, 'epresent@emeryweiner.org','agfdgf', 2, 'USB-e', 4)
-    print(userDataFrame)
+    # Testing trying to add a prexisting student
+    success = database.add_student(email='epresent@emeryweiner.org', verifyCodeIn='agfdgf', chargerNum=2, chargerType='Dell', lockerNumIn=4)
+    print('\nDatabase:', database, '\nSuccess:', success)
 
-    # Pickle the user data to its file
-    #pandas_pickle(userDataFile, userDataFrame, 'w')
+    # Testing editing student data normally
+    success = database.edit_student('epresent@emeryweiner.org', verifyCodeOut='df4ert', lockerNumOut=5, timeOut=pd.Timestamp.now())
+    print('\nDatabase:', database, '\nSuccess:', success)
 
-# Define the setup_user_data function to setup the user data dataframe
-def setup_user_data(filename):
-    # Try to get the user data from the pickle file
-    userDataFrame = pandas_pickle(filename)
+    # Testing trying to edit a non-existent student
+    success = database.edit_student('zsamuels@emeryweiner.org', verifyCodeOut='dhbgvc', lockerNumOut=3, timeOut=pd.Timestamp.now())
+    print('\nDatabase:', database, '\nSuccess:', success)
 
-    # If the dataframe is empty, then set it up completely
-    if userDataFrame.empty:
+    # Testing getting a student's data normally
+    data = database.get_student('epresent@emeryweiner.org')
+    print('\nStudent Data:', data)
 
-        # Create a list with the column names
-        column_names = ['Verification Code In', 'Charger Number', 'Charger Type', 'Locker Number', 'Timestamp In', 'Verification Code Out', 'Timestamp Out']
+    # Testing getting a student's data converted to a different data type
+    convertedData = database.get_student('epresent@emeryweiner.org', 'dict')
+    print('\nConverted Student Data:', convertedData)
 
-        # Set the columns labels of the dataframe to the column names list
-        #userDataFrame.columns = column_names
-
-        # Make a dataframe with the column names and index name
-        userDataFrame = pd.DataFrame(columns=column_names, dtype='object')
-
-        # Create a variable with the index name
-        indexName = 'Email'
-        
-        # Set the index label of the dataframe to the index name variable
-        userDataFrame.rename_axis(indexName)
-
-    # Return the user data frame
-    return userDataFrame
+    # Testing trying to get a student's data converted to a non-supported data type
+    notSupportedData = database.get_student('epresent@emeryweiner.org', 'hashMap')
+    print('\nNot Supported Conversion Student Data:', notSupportedData)
 
 
-# Define the add_user_data function to add user data to the user data dataframe
-def add_user_data(userDataFrame, email, verifyCodeIn, chargerNum, chargerType, lockerNum):
-    # Get the current timestamp
-    timestamp = pd.Timestamp.now()
-
-    # Make a pandas series to store the new data
-    newSeries = pd.Series(data={'Verification Code In': verifyCodeIn, 'Charger Number': chargerNum, 'Charger Type': chargerType, 'Locker Number': lockerNum, 'Timestamp In': timestamp}, name=email)
-
-    # Try to update the user dataframe with the newSeries
-    try:
-
-        # Append the the new series to the user dataframe
-        userDataFrame = userDataFrame.append(newSeries, verify_integrity=True)
-    
-    # Catch a ValueError if there already is a entry for the student in the dataframe
-    except ValueError:
-
-        # Return False
-        return False
-
-    # Else, return True
-    else:
-
-        # Return the userDataFrame                      #Temp should be a data attribute in a class
-        return userDataFrame
+# Define the Database class to work with the student data
+class Database():
 
 
-# Define the manual_get_dataframe method to manually open a csv file and store it in a dataframe
-def manual_get_dataframe(filename):
-    # Read the csv file using pandas
-    dataframe = pd.read_csv(filename, index_col=0)
+    # Initialize the database
+    def __init__(self, filename):
+        # Store the filename in a data attribute
+        self.__filename = filename
 
-    # Return the dataframe
-    return dataframe
+        # Set up the dataframe
+        self.setup_dataframe()
 
 
-# Define the pandas_pickle function to load from or dump to pickle files with panda objects
-def pandas_pickle(filename, pandasObject='', mode='r'):
-        # If mode is equal to read, then read the contents of the data file
-        if mode == 'r':
+    # Define the setup_dataframe method to set up the dataframe to store the student data
+    def setup_dataframe(self):
+        # Try to get the student data from the pickle file
+        self.__studentFrame = self.data_pickle()
 
-            # Try to unpickle the file
-            try:
+        # Create a list with the data names
+        self.__data_names = ['Verification Code In', 'Charger Number', 'Charger Type', 'Locker Number In', 'Timestamp In', 'Verification Code Out', 'Locker Number Out', 'Timestamp Out']
 
-                # Load the contents of the file into a pandas object
-                pandasObject = pd.read_pickle(filename)
+        # If the dataframe is empty, then set it up completely
+        if self.__studentFrame.empty:
+
+            # Make a dataframe with the data names
+            self.__studentFrame = pd.DataFrame(columns=self.__data_names)
+
+            # Store the index name in a variable
+            indexName = 'Email'
             
-            # Catch an exception if the file does not exist
-            except (OSError, IOError, EOFError):
+            # Set the index label of the dataframe to the index name variable
+            self.__studentFrame.rename_axis(indexName, inplace=True)
 
-                # Set the pandas object to an empty data frame
-                pandasObject = pd.DataFrame(dtype='object')
 
-            # Finally, return the pandas object
-            finally:
+    # Define the add_student method to add student data to the student data dataframe
+    def add_student(self, email, verifyCodeIn, chargerNum, chargerType, lockerNumIn):
+        # If the email is in the index values, then edit the student's data
+        if email not in self.__studentFrame.index:
 
-                # Return the pandas object
-                return pandasObject
+            # Get the pandas Series associated with the student's data
+            self.__studentFrame.loc[email, ['Verification Code In', 'Charger Number', 'Charger Type', 'Locker Number In', 'Timestamp In']] = verifyCodeIn, chargerNum, chargerType, lockerNumIn, pd.Timestamp.now()
+            
+            # Pickle the dataframe to its file
+            self.data_pickle('w')
+            
+            # Return True
+            return True
+        
+        # Else, return False
+        else:
 
-        # Else if mode is equal to write and a pandas object argument was supplied, 
-        # then dump the pandas object into the data file
-        elif mode == 'w' and type(pandasObject) != str:
+            return False
+        
+        '''
+        # Get the current timestamp
+        timestamp = pd.Timestamp.now()
 
-            # Dump the pandas object into the file
-            pandasObject.to_pickle(filename)
+        # Make a pandas series to store the new data
+        newSeries = pd.Series(data={'Verification Code In': verifyCodeIn, 'Charger Number': chargerNum,
+        'Charger Type': chargerType, 'Locker Number In': lockerNumIn, 'Timestamp In': timestamp}, name=email)
+
+        # Try to update the student dataframe with the new series
+        try:
+
+            # Append the new series to the student dataframe
+            self.__studentFrame = self.__studentFrame.append(newSeries, verify_integrity=True)
+        
+        # Catch a ValueError if there already is a entry for the student in the dataframe
+        except ValueError:
+
+            return False
+
+        # Else, pickle the dataframe and return True
+        else:
+
+            # Pickle the dataframe to its file
+            self.data_pickle('w')
+            
+            # Return True
+            return True
+        '''
+    
+
+    # Define the edit_student method to edit student data in the student data dataframe
+    def edit_student(self, email, verifyCodeIn=None, chargerNum=None, chargerType=None, lockerNumIn=None, timeIn=None, verifyCodeOut=None, lockerNumOut=None, timeOut=None):
+        # If the email is in the index values, then edit the student's data
+        if email in self.__studentFrame.index:
+
+            # Make a two dimensional list with all the values that should be changed
+            changeValues = [keyValuePair for keyValuePair in zip(self.__data_names,
+            (verifyCodeIn, chargerNum, chargerType, lockerNumIn, timeIn, verifyCodeOut, lockerNumOut, timeOut)) if keyValuePair[1] != None]
+
+            # For keyValuePair in the change values, update the student's data              # I realize this is inefficient = pandas can sometimes be annoying
+            for keyValuePair in changeValues:
+
+                # Update the student's data
+                self.__studentFrame.loc[email, keyValuePair[0]] = keyValuePair[1]
+
+
+            # Pickle the dataframe to its file
+            self.data_pickle('w')
+            
+            
+            # Return True
+            return True
+        
+        # Else, return False
+        else:
+
+            return False
+
+        '''
+        # Get the pandas series associated with the student's data
+        studentSeries = self.__studentFrame.loc[email]
+
+        # Make a two dimensional list turned dictionary with all the values that should be changed
+        changeValues = dict([keyValuePair for keyValuePair in zip(self.__data_names,
+        (verifyCodeIn, chargerNum, chargerType, lockerNumIn, timeIn, verifyCodeOut, lockerNumOut, timeOut)) if keyValuePair[1] != None])
+
+        # Update the pandas series
+        studentSeries.update(changeValues)
+
+        # Update the dataframe
+        self.__studentFrame.update(studentSeries)
+
+        #Or
+
+        self.__studentFrame.loc[email].update(changeValues)
+        '''
+
+
+    # Define the remove_student method to remove student data from the student data dataframe
+    def remove_student(self, email):
+        # If the email is in the index values, then remove the student's data
+        if email in self.__studentFrame.index:
+
+            # Drop the student's data out of the dataframe in place
+            self.__studentFrame.drop(index=email, inplace=True)
+
+            # Pickle the dataframe to its file
+            self.data_pickle('w')
+            
+            # Return True
+            return True
+        
+        # Else, return False
+        else:
+
+            return False
+
+
+    # Define the get_student method to get student data from the student data dataframe
+    def get_student(self, email, type='series', **kwargs):
+        # If the email is in the index values, then get the student's data
+        if email in self.__studentFrame.index:
+
+            # Get the series of data associate with the email
+            studentSeries = self.__studentFrame.loc[email]
+
+            # If the series has a to_'type' option, then return the series as that type of data
+            if hasattr(studentSeries, f'to_{type}'):
+
+                # Try to convert the series to the data type
+                try:
+
+                    # Get the method to convert the series and run it with the keyword arguments
+                    data = getattr(studentSeries, f'to_{type}')(*kwargs)
+                
+                # Catch an exception if it failed to convert
+                except:
+
+                    # Return None at the end
+                    pass
+                
+                # Else, return the data
+                else:
+
+                    # Return the data
+                    return data
+            
+            # Else if the type is series, then return it as a normal series
+            elif type == 'series':
+
+                # Return the series
+                return studentSeries
+        
+        # Return None if something did not work
+        return None
+
+
+    # Define the csv_to_dataframe method to open a csv file and store it as the dataframe
+    def csv_to_dataframe(self, filename):
+        # Read the csv file using pandas
+        dataframe = pd.read_csv(filename, index_col=0)
+
+        # Store the dataframe as the main dataframe
+        self.__studentFrame = dataframe
+
+        # Pickle the dataframe to its file
+        self.data_pickle('w')
+
+
+    # Define the data_pickle method to load from or dump to the data's pickle file
+    def data_pickle(self, mode='r'):
+            # If mode is equal to read, then read the contents of the data file
+            if mode == 'r':
+
+                # Try to unpickle the file
+                try:
+
+                    # Load the contents of the file into a pandas object
+                    pandasObject = pd.read_pickle(self.__filename)
+                
+                # Catch an exception if the file does not exist
+                except (OSError, IOError, EOFError):
+
+                    # Set the pandas object to an empty dataframe
+                    pandasObject = pd.DataFrame()
+
+                # Finally, return the pandas object
+                finally:
+
+                    # Return the pandas object
+                    return pandasObject
+
+            # Else if mode is equal to write, then dump the data into its data file
+            elif mode == 'w':
+
+                # Dump the pandas object into its file
+                self.__studentFrame.to_pickle(self.__filename)
+        
+
+    # Define the __str__ method to show the dataframe properly if an instance of the class is used as a string
+    def __str__(self):
+
+        # Return the student dataframe as a string
+        return str(self.__studentFrame)
 
 
 # When main is called
 if __name__ == '__main__':
 
-    # Call the main() function
-    main()
+    # Call the demo function
+    demo()
