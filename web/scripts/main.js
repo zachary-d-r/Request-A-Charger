a05d2b406a4322c35d127fbe2bf22f5fba0744f3
zachary-d-r <zacharydylanrobinson@gmail.com>
2021-09-27 12:24:10 -0500

    Added the third and final page

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,5 +1,9 @@
 
 document.getElementById('submit-button').addEventListener("click", ()=>{eel.getStudentEmail()}, false)
+document.getElementById('button-1').addEventListener("click", ()=>{chargerSelected(1)}, false)
+document.getElementById('button-2').addEventListener("click", ()=>{chargerSelected(2)}, false)
+document.getElementById('button-3').addEventListener("click", ()=>{chargerSelected(3)}, false)
+document.getElementById('button-4').addEventListener("click", ()=>{chargerSelected(4)}, false)
 
 // Get the student ID from the textbox
 eel.expose(getStudentEmailJS)
@@ -20,18 +24,31 @@ function getVerificationCodeJS() {
     return document.getElementById('email-input').value
 }
 
-// Animation from verification to what locker number you take out
+// Animation from verification to what charger select screen
 eel.expose(secondAnimation)
 function secondAnimation() {
     document.getElementById('login-div').className = 'login-div-animate-2'
-    document.getElementById('title').className = 'title-animate-2'
-    document.getElementById('title').innerHTML = 'Your Charger is In'
-    document.getElementById('subtitle').innerHTML = 'LOCKER NUMBER'
     document.getElementById('submit-button').className = 'signin-button-animate-2'
-    document.getElementById('submit-button').innerHTML = 'Done'
     document.getElementById('email-input').placeholder = ''
     document.getElementById('email-input').style.visibility = 'hidden'
     document.getElementById('email').className = 'number-box-2'
+    document.getElementById('title').innerHTML = 'Select a Charger'
+    document.getElementById('subtitle').innerHTML = 'TO USE FOR YOUR COMPUTER'
+    document.getElementById('buttons').className = 'show-button'
+
+}
+
+// Animation from verification to what locker number you take out
+eel.expose(thirdAnimation)
+function thirdAnimation() {
+    document.getElementById('login-div').className = 'login-div-animate-3'
+    document.getElementById('buttons').className = 'show-button-animate-3'
+    document.getElementById('title').className = 'title-animate-3'
+    document.getElementById('title').innerHTML = 'Your Charger is In'
+    document.getElementById('subtitle').innerHTML = 'LOCKER NUMBER'
+    document.getElementById('submit-button').className = 'signin-button-animate-3'
+    document.getElementById('submit-button').innerHTML = 'Done'
+    document.getElementById('email').className = 'number-box-3'
 
 }
 
@@ -44,4 +61,8 @@ function firstAnimation() {
     document.getElementById('email').className = 'email-animate'
     document.getElementById('submit-button').className = 'signin-button-animate'
     document.getElementById('email-input').placeholder = 'Check your email for code'
+}
+
+function chargerSelected(num) {
+    thirdAnimation()
 }
